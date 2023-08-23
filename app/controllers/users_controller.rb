41cb28a99a2b0aa5d5a6e6b33b16eac3c79ede1c
Ngọc Tuân <nguyen.ngoc.tuan-c@sun-asterisk.com>
2023-08-23 16:52:49 +0700

    chapter_7

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,31 @@
+class UsersController < ApplicationController
+  def show
+    @user = User.find_by id: params[:id]
+    return if @user
+
+    flash[:danger] = t("user_not_found")
+    redirect_to static_pages_home_path
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      # Handle a successful save.
+      flash[:success] = t("user_created_successfully")
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit :name, :email, :password,
+                                 :password_confirmation
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   get "static_pages/home"
   get "static_pages/help"
   get "static_pages/contact"
+
+  get "/signup", to: "users#new"
+  post "/signup", to: "users#create"
+  resources :users, only: :show
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
