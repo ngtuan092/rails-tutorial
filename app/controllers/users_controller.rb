2a37a91833411911cc5c80a147e33128aa6f4a07
Ngọc Tuân <nguyen.ngoc.tuan-c@sun-asterisk.com>
2023-08-23 16:52:49 +0700

    chapter 8

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,7 @@ class UsersController < ApplicationController
     @user = User.new user_params
     if @user.save
       # Handle a successful save.
+      log_in @user
       flash[:success] = t("user_created_successfully")
       redirect_to @user
     else
